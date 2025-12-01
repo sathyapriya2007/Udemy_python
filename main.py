b8769e47f738ff3c5b375b5c39ba70215f04c33e
sathyapriya2007 <sathyapriya4774@gmail.com>
2025-12-01 11:35:58 +0530

    password manager

--- main.py
+++ main.py
@@ -1,29 +1,60 @@
 from tkinter import *
 
-BACKGROUND_COLOUR ="#B1DDC6"
+# ------------------------ PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD -------------------------------
+
+def save():
+
+    website = website_entry.get()
+    email =email_entry.get()
+    password =password_entry.get()
+
+    with open("data.txt","a") as data_file:
+        data_file.write(f"{website}| {email} |{password}")
 
 
 
+# ---------------------------- UI SETUP ------------------------------- #
 
 window =Tk()
-window.title("Flashy")
-window.config(padx=50,pady=50,bg=BACKGROUND_COLOUR)
+window.title("Password Manager")
+window.config(padx=20,pady=20)
+canvas =Canvas(height=200,width=200)
+logo_img =PhotoImage(file="logo.png")
+canvas.create_image(100,100,image=logo_img)
+canvas.grid(row=0,column=1)
 
-canvas =Canvas(width=500,height=526)
-card_front_img =PhotoImage(file="images/card_front.png")
-canvas.create_image(400,263,image=card_front_img)
-canvas.create_text(400,150,text="Title",font=("Ariel",40,"italic"))
-canvas.create_text(400,263,text="word",font=("Ariel",60,"bold"))
-canvas.config(bg=BACKGROUND_COLOUR,highlightthickness=0)
-canvas.grid(row=0,column=0)
 
-cross_image =PhotoImage(file="images/wrong.png")
-unknown_button =Button(image=cross_image,highlightthickness=0)
-unknown_button.grid(row=1,column=0,columnspan=2)
+#Labels
+website_label =Label(text="Website:")
+website_label.grid(row=1,column=0)
+email_label =Label(text="Email/Username:")
+email_label.grid(row=2,column=0)
+password_label =Label(text="Password:")
+password_label.grid(row=3,column=0)
 
-check_image =PhotoImage(file="images/right.png")
-known_button =Button(image=check_image,highlightthickness=0)
-known_button.grid(row=1,column=1)
+#Entries
+website_entry =Entry(width=35)
+website_entry.grid(row=1,column=1,columnspan=2)
+website_entry.focus()
+email_entry =Entry(width=35)
+email_entry.grid(row=2,column=1,columnspan=2)
+email_entry.insert(0 ,"sathya@gmail.com")
+password_entry =Entry(width=21)
+password_entry.grid(row=3,column=1)
+
+#Buttons
+generate_password_button =Button(text="Generate Password")
+generate_password_button.grid(row=3,column=2)
+add_button =Button(text="Add",width=34,command=save)
+add_button.grid(row=4,column=1,columnspan=2)
+
+
+
+
 
 window.mainloop()
+
+
 
