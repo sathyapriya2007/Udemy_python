124598f20dfef5d84b4d0b7e324ee8e4b283e2ff
sathyapriya2007 <sathyapriya4774@gmail.com>
2025-12-01 12:15:40 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,68 +1,91 @@
 import requests
-from datetime import datetime
 
-USERNAME = "sathya"
-TOKEN = "kmawporqnJFIQJFOq"
-GRAPH_ID = "graph1"
+STOCK_NAME = "TSLA"
+COMPANY_NAME = "Tesla Inc"
 
-pixela_endpoint = "https://pixe.la/v1/users"
+STOCK_ENDPOINT = "https://www.alphavantage.co/query"
+NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+NEWSAPI
+STOCK_API_KEY ="P7FEEGDXXEC0BSTM"
 
-user_params = {
-    "token": TOKEN,
-    "username": USERNAME,
-    "agreeTermsOfService": "yes",
-    "notMinor": "yes",
+    ## STEP 1: Use https://www.alphavantage.co/documentation/#daily
+# When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+
+#TODO 1. - Get yesterday's closing stock price. Hint: You can perform list comprehensions on Python dictionaries. e.g. [new_value for (key, value) in dictionary.items()]
+stock_params ={
+     "function": "TIME_SERIES_DAILY",
+     "symbol": STOCK_NAME,
+     "apikey": STOCK_API_KEY,
 }
 
-## POST
-# response = requests.post(url=pixela_endpoint, json=user_params)
-# print(response.text)
-
-graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
-
-graph_config = {
-    "id": "graph1",
-    "name": "Cycling Graph",
-    "unit": "Km",
-    "type": "float",
-    "color": "ajisai"
-}
+response =requests.get(STOCK_ENDPOINT, params =stock_params)
+data = response.json()["Time Series(Daily)"]
+data_list =[value for(key, value)  in data.items()]
+yesterday_data =data_list[0]
+yesterday_closing_price =yesterday_data["4,close"]
+print(yesterday_closing_price)
 
 
-headers = {
-    "X-USER-TOKEN": TOKEN
-}
-
-response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
-print(response.text)
-
-pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
-
-today = datetime.now()
-# print(today.strftime("%Y%m%d"))
-
-pixel_data = {
-    "date": tofday.strftime("%Y%m%d"),
-    "quantity": "15",
-}
-
-response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
-print(response.text)
-
-update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
-
-new_pixel_data = {
-    "quantity": "4.5"
-}
-
-## PUT
-# response = requests.put(url=update_endpoint, json=new_pixel_data, headers=headers)
-# print(response.text)
 
 
-delete_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
 
 
-## DELETE
-# response = requests.delete(url=delete_endpoint, headers=headers)
-# print(response.text)
+
+
+#TODO 2. - Get the day before yesterday's closing stock price
+day_before_yesterday_data =data_list[1]
+day_before_yesterday_closing_price =day_before_yesterday_data["4.close"]
+print(day_before_yesterday_closing_price)
+
+#TODO 3. - Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
+difference =abs(float(yesterday_closing_price)-float(day_before_yesterday_closing_price))
+
+print(difference)
+#TODO 4. - Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday.
+diff_percent =(difference/float(yesterday_closing_price))*100
+print(diff_percent)
+
+
+
+#TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
+if diff_percent >5:
+    print("Get News")
+    ## STEP 2: https://newsapi.org/ 
+    # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
+
+#TODO 6
+if diff_percent >1:
+    news_params ={
+        "apiKey": NEWS_API_KEY,
+        "qInTitle": COMPANY_NAME,
+    }
+    news_response =requests.get(NEWS_ENDPOINT,params=news_params)
+    articles =news_response.json()["articles"]
+    print(articles)
+#TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
+ three_articles =articles[:3]
+ print(three_articles)
+
+    ## STEP 3: Use twilio.com/docs/sms/quickstart/python
+    #to send a separate message with each article's title and description to your phone number. 
+
+#TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
+   formated_articles =[f"Headline:{article['title']}.\nBrief:{article['description']}" for article in three_articles]
+
+
+
+#TODO 9. - Send each article as a separate message via Twilio. 
+
+
+
+#Optional TODO: Format the message like this: 
+"""
+TSLA: 🔺2%
+Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
+Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
+or
+"TSLA: 🔻5%
+Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
+Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
+"""
+
