f26544a81cece73664403bb7263c5dc3a7f88814
sathyapriya2007 <sathyapriya4774@gmail.com>
2025-12-01 12:09:56 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,60 +1,108 @@
 from tkinter import *
+import math
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 1
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+reps = 0
+timer = None
 
-# ------------------------ PASSWORD GENERATOR ------------------------------- #
+# ---------------------------- TIMER RESET ------------------------------- # 
 
-# ---------------------------- SAVE PASSWORD -------------------------------
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    title_label.config(text="Timer")
+    check_marks.config(text="")
+    global reps
+    reps = 0
 
-def save():
 
-    website = website_entry.get()
-    email =email_entry.get()
-    password =password_entry.get()
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
 
-    with open("data.txt","a") as data_file:
-        data_file.write(f"{website}| {email} |{password}")
+def start_timer():
+    global reps
+    reps += 1
 
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        title_label.config(text="Break", fg=RED)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        title_label.config(text="Break", fg=PINK)
+    else:
+        count_down(work_sec)
+        title_label.config(text="Work", fg=GREEN)
+
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def count_down(count):
+
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        global timer
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        marks = ""
+        work_sessions = math.floor(reps/2)
+        for _ in range(work_sessions):
+            marks += "✔"
+        check_marks.config(text=marks)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
-
-window =Tk()
-window.title("Password Manager")
-window.config(padx=20,pady=20)
-canvas =Canvas(height=200,width=200)
-logo_img =PhotoImage(file="logo.png")
-canvas.create_image(100,100,image=logo_img)
-canvas.grid(row=0,column=1)
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
 
 
-#Labels
-website_label =Label(text="Website:")
-website_label.grid(row=1,column=0)
-email_label =Label(text="Email/Username:")
-email_label.grid(row=2,column=0)
-password_label =Label(text="Password:")
-password_label.grid(row=3,column=0)
+title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
+title_label.grid(column=1, row=0)
 
-#Entries
-website_entry =Entry(width=35)
-website_entry.grid(row=1,column=1,columnspan=2)
-website_entry.focus()
-email_entry =Entry(width=35)
-email_entry.grid(row=2,column=1,columnspan=2)
-email_entry.insert(0 ,"sathya@gmail.com")
-password_entry =Entry(width=21)
-password_entry.grid(row=3,column=1)
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
 
-#Buttons
-generate_password_button =Button(text="Generate Password")
-generate_password_button.grid(row=3,column=2)
-add_button =Button(text="Add",width=34,command=save)
-add_button.grid(row=4,column=1,columnspan=2)
+start_button = Button(text="Start", highlightthickness=0, command=start_timer)
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset", highlightthickness=0, command=reset_timer)
+reset_button.grid(column=2, row=2)
+
+check_marks = Label(fg=GREEN, bg=YELLOW)
+check_marks.grid(column=1, row=3)
+
 
 
 
 
 
 window.mainloop()
+
+
+
+
+
+
+
+
 
 
 
