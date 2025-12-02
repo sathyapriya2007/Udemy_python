e330a0f96b53d413fdc7393ed3a5ab79b982ec85
sathyapriya2007 <sathyapriya4774@gmail.com>
2025-12-02 19:34:35 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,47 +1,34 @@
-from bs4 import BeautifulSoup
-import requests
-import spotipy
-from spotipy.oauth2 import SpotifyOAuth
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 
-# Scraping Billboard 100
-date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
-header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0"}
-billboard_url = "https://www.billboard.com/charts/hot-100/" + date
-response = requests.get(url=billboard_url, headers=header)
+# Deprecated - no longer needed
+chrome_driver_path = "/Users/philippmuellauer/Development/chromedriver"
 
-soup = BeautifulSoup(response.text, 'html.parser')
-song_names_spans = soup.select("li ul li h3")
-song_names = [song.getText().strip() for song in song_names_spans]
+# keeps chrome open
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
 
-# Spotify Authentication
-sp = spotipy.Spotify(
-    auth_manager=SpotifyOAuth(
-        scope="playlist-modify-private",
-        redirect_uri="http://example.com",
-        client_id=YOUR-CLIENT-ID,
-        client_secret=YOUR-CLIENT-SECRET,
-        show_dialog=True,
-        cache_path="token.txt"
-    )
-)
-user_id = sp.current_user()["id"]
-print(user_id)
+# driver = webdriver.Chrome(executable_path=chrome_driver_path)
+# driver = webdriver.Chrome()
+driver = webdriver.Chrome(options=chrome_options)
 
-# Searching Spotify for songs by title
-song_uris = []
-year = date.split("-")[0]
-for song in song_names:
-    result = sp.search(q=f"track:{song} year:{year}", type="track")
-    print(result)
-    try:
-        uri = result["tracks"]["items"][0]["uri"]
-        song_uris.append(uri)
-    except IndexError:
-        print(f"{song} doesn't exist in Spotify. Skipped.")
+def test_eight_components():
+    driver.get("https://www.selenium.dev/selenium/web/web-form.html")
+    title = driver.title
+    assert title == "Web form"
+    driver.implicitly_wait(0.5)
+    text_box = driver.find_element(by=By.NAME, value="my-text")
+    submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
+    text_box.send_keys("Selenium")
+    submit_button.click()
+    message = driver.find_element(by=By.ID, value="message")
+    value = message.text
+    assert value == "Received!"
 
-# Creating a new private playlist in Spotify
-playlist = sp.user_playlist_create(user=user_id, name=f"{date} Billboard 100", public=False)
-print(playlist)
+    # Closes Chrome
+    # driver.quit()
+    driver.close()
 
-# Adding songs found into the new playlist
-sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
+
+test_eight_components()
