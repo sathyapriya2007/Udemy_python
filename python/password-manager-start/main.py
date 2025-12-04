ba847f564ef5198fd8038559b29ca31b3a956cef
sathyapriya2007 <sathyapriya4774@gmail.com>
2025-12-04 19:57:54 +0530

    new files are exist

--- python/password-manager-start/main.py
+++ python/password-manager-start/main.py
@@ -21,7 +21,7 @@ window =Tk()
 window.title("Password Manager")
 window.config(padx=20,pady=20)
 canvas =Canvas(height=200,width=200)
-logo_img =PhotoImage(file="logo.png")
+logo_img =_tkinterPhotoImage(file="logo.png")
 canvas.create_image(100,100,image=logo_img)
 canvas.grid(row=0,column=1)
 
